47f0cb05a20c2dded3b42eeaabdab13c21e30889
camilasatie <camilasatie.dev@gmail.com>
2020-07-28 21:31:49 -0300

    chore: add storybook knobs

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
   addons: [
     '@storybook/preset-typescript',
     '@storybook/addon-actions',
-    '@storybook/addon-links'
+    '@storybook/addon-links',
+    '@storybook/addon-knobs/register'
   ],
 };
