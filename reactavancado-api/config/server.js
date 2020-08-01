94f41146d4f2e222233f42dfbec02b241e752068
camilasatie <camilasatie.dev@gmail.com>
2020-08-01 12:42:31 -0300

    chore: add strapi api

---
+++ reactavancado-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '93361b77c491618c116da778c951a183'),
+    },
+  },
+});
---
+++ reactavancado-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ce471ed0-39cc-4713-9217-22793fbf58a2'
+};
