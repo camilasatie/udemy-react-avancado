90e30290c3b261ee75dc8ec9c64f918873b64fa6
camilasatie <camilasatie.dev@gmail.com>
2020-08-02 17:48:46 -0300

    chore: fetch data and env for development

---
+++ landing-page/src/graphql/queries/getLandingPage.ts
@@ -0,0 +1,11 @@
+const GET_LANDING_PAGE = /* GraphQL */ `
+  query GET_LANDING_PAGE {
+    landingPage {
+      logo {
+        url
+        alternativeText
+      }
+    }
+  }
+`
+export default GET_LANDING_PAGE
